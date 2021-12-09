1bbd5f8b1e6eff340b9a80a198d5a881eced5c9d
Axelle-Ga <axelle.gaige@gmail.com>
2021-12-09 11:10:16 +0000

    feat(menucontroller): add deleteMenu

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -5,14 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.cicdlectures.menuserver.dto.MenuDto;
 import com.cicdlectures.menuserver.service.CreateMenuService;
 import com.cicdlectures.menuserver.service.ListMenuService;
+
+import com.cicdlectures.menuserver.repository.MenuRepository;
 
 @RestController
 public class MenuController {
@@ -21,10 +26,13 @@ public class MenuController {
 
   private final ListMenuService listMenuService;
 
+  private final MenuRepository menuRepository;
+
   @Autowired
-  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService) {
+  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService, MenuRepository menuRepository) {
     this.createMenuService = createMenuService;
     this.listMenuService = listMenuService;
+    this.menuRepository = menuRepository;
   }
 
   @GetMapping(path = "/menus", produces = "application/json")
@@ -37,4 +45,19 @@ public class MenuController {
   public MenuDto createMenu(@RequestBody MenuDto menu) {
     return createMenuService.createMenu(menu);
   }
+
+  @DeleteMapping(path = "/menus/{id}")
+  public ResponseEntity<Long>  deleteMenu(@PathVariable Long id) {
+    if (!menuRepository.existsById(id)) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);    
+    }
+    else{
+      menuRepository.deleteById(id);
+    }
+    
+    return new ResponseEntity<>(id, HttpStatus.OK);
+    
+  }
+
+  
 }
